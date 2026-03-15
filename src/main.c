30e89af744d71bd1daeaaa57c897d1bec7c91c6d
foxtrot222 <foxtrot25012@proton.me>
2026-03-15 15:55:53 +0530

    feat: add cmd arguments

--- src/main.c
+++ src/main.c
@@ -16,6 +16,37 @@ int main(int argc, char **argv) {
   int delay = 100;
   bool toroidal = true;
   
+  while ((opt = getopt(argc, argv, "s:th")) != -1) {
+    switch (opt) {
+      case 'h':
+	printf("Usage: %s [-d delay] [-t] [-h]\n", argv[0]);
+	printf(" -d DELAY    Simulation delay time in milliseconds. Default is 100\n");
+	printf(" -t          To toggle Toroidal edges. Default is false\n");
+        printf(" -h          Show this help message and exit\n");
+	printf("How to Play:\n");
+	printf("  Use the arrow keys to move the cursor.\n");
+	printf("  Press a to toggle cells (make them alive).\n");
+	printf("  Press d to toggle cells (make them dead).\n");
+	printf("  Once your pattern is ready, press Enter to start the simulation.\n");
+	printf("  Press q at any time to quit.\n");
+        return 0;
+      case 't':
+        toroidal = true;
+	break;
+      case 's':
+        delay_tmp = atoi(optarg);
+        if (delay_tmp <= 0) {
+	  fprintf(stderr, "Invalid Input\n");
+	  return 1;
+        }
+	delay = delay_tmp;
+        break;
+      default:
+        fprintf(stderr, "Unknown option\n");
+	return 1;
+    }
+  }
+  
   init_ncurses();
   int rows, cols;
   getmaxyx(stdscr, rows, cols);
